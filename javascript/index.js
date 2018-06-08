a5ec91b3f7014675443ecf4d94428b7955b92801
blucheez <nathanalam99@gmail.com>
2018-06-08 10:00:53 -0400

    hmm

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,17 @@
-console.log("The index is responding");
+const http = require('http');
+const url = require('url');
 
-var http = require("http");
+const hostname = '0.0.0.0';
+const port = process.env.PORT || 3000;
 
-try {
-  http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type":"text/html"});
-    response.write("Hello World");
-    response.end();
-  }).listen(80);
-} catch (e) {
-  console.log(e);
-}
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    const path = url.parse(req.url).pathname.slice(1).replace(/%20/g, ' ')
+
+    res.writeHead(200, {'Content-Type': 'application/json'})
+    res.write("ayy lmao though");
+    res.end();
+  }
+}).listen(port, hostname, function(){
+  console.log('Server running at http://${' + hostname + '}:${' + port + '}/');
+});

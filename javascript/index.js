d1d0a19aee83227506c6f8164419fd4b4c73db06
blucheez <nathanalam99@gmail.com>
2018-06-08 09:42:33 -0400

    basic server

---
+++ javascript/index.js
@@ -0,0 +1,10 @@
+console.log("The index is responding");
+
+var http = require("http");
+
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type":"text/html"});
+  response.write("Hello World");
+  response.end();
+}).listen(80);

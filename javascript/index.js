a46993b17f13bd6f092062909e0769ce3c73b00a
blucheez <nathanalam99@gmail.com>
2018-06-08 13:30:54 -0400

    username password

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,26 @@
 // framework for handling http interactions
 var app = require("express")();
 
+// get rid of the /javascript
+var path = __dirname.substring(0, __dirname.length - 10);
+
 // port used for heroku app
 var port = process.env.PORT || 3000;
 
+// allow us to parse form data
+app.use(require("express-formidable")());
+
 app.get("/", function(req, res) {
-  res.send("hello world");
+  res.sendFile(path + "/html/index.html");
 });
 
 app.get("/image", function(req, res) {
-  res.sendFile(__dirname + "/hpton.jpg");
+  res.sendFile(path + "/hpton.jpg");
+});
+
+app.post("/data", function(req, res) {
+  console.log(req.fields);
+  res.send(req.fields);
 });
 
 app.listen(port, function() {

c58709f46436b9b4e89c0848ae6a45fb58226bec
blucheez <nathanalam99@gmail.com>
2018-06-08 09:48:52 -0400

    fix

--- javascript/index.js
+++ javascript/index.js
@@ -2,9 +2,12 @@ console.log("The index is responding");
 
 var http = require("http");
 
-
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type":"text/html"});
-  response.write("Hello World");
-  response.end();
-}).listen(80);
+try {
+  http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type":"text/html"});
+    response.write("Hello World");
+    response.end();
+  }).listen(80);
+} catch (e) {
+  console.log(e);
+}
